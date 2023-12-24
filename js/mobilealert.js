04e9e2a781d7e816b47d12f396324eee183daf50
intosins <148978945+intosins@users.noreply.github.com>
2023-12-24 13:58:26 +0200

    Delete js/mobilealert.js

--- js/mobilealert.js
+++
@@ -1,3 +0,0 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    alert("Please note that our website is not optimized for mobile devices. For the best experience, we recommend using a desktop browser.");
-}
